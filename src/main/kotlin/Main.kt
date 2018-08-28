a1e81241c4e672a3694a5fcd05f3365952252552
Innocent <31923567+angwandi@users.noreply.github.com>
2018-08-28 20:59:02 +0100

    Add Variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,15 @@
 fun main(args: Array<String>) {
     println("Hello World")
     println("My first Kotlin program")
+    /*Variable: name given to an area of memory so that we can refer to content by name */
+    var tim: String
+    /*Type of variable tim is a String */
+    tim = "Tim"
+    println(tim)
+    var timSalary: Int = 32
+    var monthly: Int = timSalary * 4
+    println(timSalary)
+    println(monthly)
+
+
 }
