75c38730a3fad1857866a9326af236480aee686c
Innocent <31923567+angwandi@users.noreply.github.com>
2018-08-28 22:02:37 +0100

    Add String \$ Interpolation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,37 +2,48 @@ fun main(args: Array<String>) {
     println("Hello World")
     println("My first Kotlin program")
     /*Variable: name given to an area of memory so that we can refer to content by name */
-    var tim: String
+    val jim = "James T Kirt"
+    val tim = "Tim Butchalka"
     /*Type of variable tim is a String */
-    tim = "Tim Butchalka"
     println(tim)
-    var timsWeeklySalary: Int = 32
-    var timsMonthlySalary: Int = timsWeeklySalary * 4
+    val timsWeeklySalary = 32
+    val timsMonthlySalary = timsWeeklySalary * 4
     println(timsWeeklySalary)
     println(timsMonthlySalary)
     /*Variable types*/
     /*Kotlin convention: start function and variable names with lower case and follow the camelCase*/
 
     println()
-    val apples: Int = 6
-    val oranges: Int = 5
-    val fruits: Int = apples - oranges
+    val apples = 6
+    val oranges = 5
+    val fruits = apples - oranges
     println(fruits)
     println(apples / 4)
 
     println()
-    val weeks: Int = 234
-    val years: Double = weeks / 52.0
-    println("130 weeks in year is: " + years)
+    val weeks = 234
+    val years = weeks / 52.0
     /*String Concatenation and Interpolation*/
-    println("My name is " + tim)
+    println("130 weeks in year is: $years")
+    println("My name is $tim")
 /*
     Interpolation in Kotlin
 */
     println("My name is $tim")
     println("$weeks weeks in year $years years")
     println("A quarter of the apple is ${apples / 4}")
+/*Challenges */
+    println("$tim has a weekly salary of : $timsWeeklySalary$")
+    println()
+    println("$tim has a monthly salary of : \$${timsWeeklySalary * 4}$")
+    println()
+    println("The difference between 6 apples and 5 oranges is : ${apples - oranges}")
+    println("$apples apples - $oranges oranges leaves $fruits piece(s) of fruits")
 
+    /*val and var: Variable declaration
+    * val used for variable we are not prepared to change or reassigned throughout the application and
+    * var can be modified
+    * */
 
-
+    println("$jim is the CEO")
 }
