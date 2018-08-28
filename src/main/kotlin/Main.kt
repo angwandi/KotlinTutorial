49a9cb7f2165b45d7fdb2ca3a0896e32fee0781e
Innocent <31923567+angwandi@users.noreply.github.com>
2018-08-28 21:11:20 +0100

    Add Variables calculations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,10 +6,21 @@ fun main(args: Array<String>) {
     /*Type of variable tim is a String */
     tim = "Tim"
     println(tim)
-    var timSalary: Int = 32
-    var monthly: Int = timSalary * 4
-    println(timSalary)
-    println(monthly)
+    var timsWeeklySalary: Int = 32
+    var timsMonthlySalary: Int = timsWeeklySalary * 4
+    println(timsWeeklySalary)
+    println(timsMonthlySalary)
+    /*Variable types*/
+    /*Kotlin convention: start function and variable names with lower case and follow the camelCase*/
+    println()
+    val apples: Int = 6
+    val oranges: Int = 5
+    val fruits: Int = apples - oranges
+    println(fruits)
+    println(apples / 4)
 
-
+    println()
+    val weeks: Int = 130
+    val years: Double = weeks / 52.0
+    println(years)
 }
