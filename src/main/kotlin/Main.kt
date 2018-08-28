8bac1c3cb67a51812830cfae326e6b3268ed9cc5
Innocent <31923567+angwandi@users.noreply.github.com>
2018-08-28 21:23:16 +0100

    Add String Concatenation and Interpolation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ fun main(args: Array<String>) {
     /*Variable: name given to an area of memory so that we can refer to content by name */
     var tim: String
     /*Type of variable tim is a String */
-    tim = "Tim"
+    tim = "Tim Butchalka"
     println(tim)
     var timsWeeklySalary: Int = 32
     var timsMonthlySalary: Int = timsWeeklySalary * 4
@@ -12,6 +12,7 @@ fun main(args: Array<String>) {
     println(timsMonthlySalary)
     /*Variable types*/
     /*Kotlin convention: start function and variable names with lower case and follow the camelCase*/
+
     println()
     val apples: Int = 6
     val oranges: Int = 5
@@ -20,7 +21,18 @@ fun main(args: Array<String>) {
     println(apples / 4)
 
     println()
-    val weeks: Int = 130
+    val weeks: Int = 234
     val years: Double = weeks / 52.0
-    println(years)
+    println("130 weeks in year is: " + years)
+    /*String Concatenation and Interpolation*/
+    println("My name is " + tim)
+/*
+    Interpolation in Kotlin
+*/
+    println("My name is $tim")
+    println("$weeks weeks in year $years years")
+    println("A quarter of the apple is ${apples / 4}")
+
+
+
 }
